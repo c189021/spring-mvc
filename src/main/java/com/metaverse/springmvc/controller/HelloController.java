adbe5f571b10cb0bbe14083347a318ee83743a58
SeoungJin Park <sjjw1111@naver.com>
2025-07-04 15:35:16 +0900

    feat: @RequestMapping을 통한 엔드포인트 공통 부분 중복 제거

--- src/main/java/com/metaverse/springmvc/controller/HelloController.java
+++ src/main/java/com/metaverse/springmvc/controller/HelloController.java
@@ -4,33 +4,34 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/api") // 상위 클래스인 컨트롤러에다가 api라는 prefix를 설정해주면 접두사 역할 할 수 있음
 public class HelloController {
 
-    @GetMapping("api/hello") //"localhost:8080/api/hello" 이렇게 하면 작동 안됨 규칙임.
+    @GetMapping("/hello") //"localhost:8080/api/hello" 이렇게 하면 작동 안됨 규칙임.
     @ResponseBody
     public String hello() {
         return "Hello World!";
     }
 
-    @GetMapping("api/get")
+    @GetMapping("/get")
     @ResponseBody
     public String get() {
         return "Get Method 요청";
     }
 
-    @PostMapping("api/post")
+    @PostMapping("/post")
     @ResponseBody
     public String post() {
         return"Post Method 요청";
     }
 
-    @PutMapping("api/put")
+    @PutMapping("/put")
     @ResponseBody
     public String put() {
         return "Put Method 요청";
     }
 
-    @DeleteMapping("api/delete")
+    @DeleteMapping("/delete")
     @ResponseBody
     public String delete() {
         return "Delete Method 요청";
