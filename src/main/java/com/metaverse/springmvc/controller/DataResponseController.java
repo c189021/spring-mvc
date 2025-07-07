73f41fdcaacda6ce7bf74ad24c08c405a5fae516
SeoungJin Park <sjjw1111@naver.com>
2025-07-07 15:12:20 +0900

    feat: JSON Java 객체 반환

--- src/main/java/com/metaverse/springmvc/controller/DataResponseController.java
+++ src/main/java/com/metaverse/springmvc/controller/DataResponseController.java
@@ -1,14 +1,21 @@
 package com.metaverse.springmvc.controller;
 
+import com.metaverse.springmvc.domain.Star;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DataResponseController {
-    @GetMapping("reponse/json/string")
+    @GetMapping("response/json/string")
     @ResponseBody
     public String helloStringJson(){
         return "{\"name\" : \"Meta\", \"age\" : 95}";
+    }
+
+    @GetMapping("response/json/instance")
+    @ResponseBody
+    public Star helloInstanceJson(){
+        return new Star("Meta",88);
     }
 }
