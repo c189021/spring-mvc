e8ebb8f963dca1c379e2a7f5fe84c0214d945061
SeoungJin Park <sjjw1111@naver.com>
2025-07-07 10:34:43 +0900

    feat: 뷰템플릿엔진 - 정적 웹 페이지 반환

--- src/main/java/com/metaverse/springmvc/controller/HelloController.java
+++ src/main/java/com/metaverse/springmvc/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api") // 상위 클래스인 컨트롤러에다가 api라는 prefix를 설정해주면 접두사 역할 할 수 있음
 public class HelloController {
 
-    @GetMapping("/hello") //"localhost:8080/api/hello" 이렇게 하면 작동 안됨 규칙임.
+    @GetMapping("/hello") //"localhost:8080/api/hello" 이렇게 하면 작동 안됨 규칙임. localhost:8080/여기까지는 써있다는 느낌
     @ResponseBody
     public String hello() {
         return "Hello World!";
