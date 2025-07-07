7c018b49f1b0600f14533fb668f2bf5b63ef468e
SeoungJin Park <sjjw1111@naver.com>
2025-07-07 12:38:07 +0900

    feat: 뷰템플릿엔진 - Model 객체를 통한 동적데이터바인딩

--- src/main/java/com/metaverse/springmvc/controller/HellowViewController.java
+++ src/main/java/com/metaverse/springmvc/controller/HellowViewController.java
@@ -1,10 +1,14 @@
 package com.metaverse.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HellowViewController {
+    private static long visitCount = 0;
+
+
     @GetMapping("/static-hello")
     public String hello() {
         return "hello.html";
@@ -22,4 +26,13 @@ public class HellowViewController {
         // suffix .html(접미사)
         return "hello-template";
     }
+
+    @GetMapping("html/dynamic")
+    public String htmlDynamic(Model model) {
+        visitCount++;
+        model.addAttribute("visits",visitCount);
+        return "hello-dynamic";
+    }
+
+
 }
